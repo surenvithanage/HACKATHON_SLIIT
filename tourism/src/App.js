85fc579b22032e61fcfa9556079b6ae35435e1c9
suren <surenanthonyvithanage@gmail.com>
2019-04-10 19:00:40 +0530

    7.00

--- tourism/src/App.js
+++ tourism/src/App.js
@@ -4,16 +4,15 @@ import Header from "./pages/partials/header";
 import Footer from "./pages/partials/footer";
 import Places from "./pages/partials/places";
 import Navbar from "./pages/partials/navbar";
-import Hotel from "./pages/hotel/hotel";
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
           <Navbar/>
-        {/*<Header/>*/}
-        {/*<Places/>*/}
-        <Hotel/>
+        <Header/>
+        <Places/>
         <Footer/>
       </div>
     );
